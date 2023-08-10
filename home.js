ca76aeb726bf43dff9293ec7fdd679b2f4ecbce5
Hmusaab <hafizmusaab8@gmail.com>
2023-08-10 00:47:48 -0800

    proj

---
+++ home.js
@@ -0,0 +1,16 @@
+const products = [...document.querySelectorAll('.products')];
+const nxtbtn = [...document.querySelectorAll('.nextbtn')];
+const prevbtn = [...document.querySelectorAll('.prevbtn')];
+
+products.forEach((items,i)=>{
+    let productsDimension = items.getBoundingClientRect();
+    let productsWidth = productsDimension.width;
+    nxtbtn[i].addEventListener('click',()=>{
+        items.scrollLeft += productsWidth;
+    })
+    
+    prevbtn[i].addEventListener('click',()=>{
+        items.scrollLeft -= productsWidth;
+    })
+
+})
